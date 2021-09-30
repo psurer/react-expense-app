11f6733a08d290c512a3bc3ff7fa983649eb60bb
psurer <surereker@icloud.com>
2021-09-30 18:07:25 +0200

    creating expensescomponent

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseDate from './components/ExpenseDate';
+import Expenses from './components/Expenses';
 function App() {
   const expenses = [
-    {
+    { 
+      id: 'e1',
       title: "rent",
       amount: 3000,
       date: new Date(2021, 1, 1),
     },
     {
+      id: 'e2',
       title: "travel",
       amount: 5000,
       date: new Date(2021, 3, 11),
     },
     {
+      id: 'e3',
       title: "car insurance",
       amount: 150,
       date: new Date(2021, 4, 21),
     },
     {
+      id: 'e4',
       title: "food",
       amount: 600,
       date: new Date(2021, 1, 16),
     },
   ];
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem 
-      title={expenses[0].title} 
-      amount={expenses[0].amount}
-      date={expenses[0].date}>
-      </ExpenseItem>
+    <div className="expenses">
       <ExpenseItem
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-      title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,23 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 //here we say that props, is the object that holds the values we get for
 // the attributes on our custom element
 // we get key - value pairs that is passed in by react automatically
 function ExpenseItem(props) {
   // using builtin date consturctor that comes with JS 
-  // here we create dummy data
-  const month  = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', { day: '2-digit'});
-  const year = props.date.getFullYear();
-
   return (
     //we use className because class is a reserved word in JS 
     // here we replace hardcoded data with opening and closing curly braces
     // built in method .toISOString is avail on all date objects to output as string
-    <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
-      <div className="expense-item__description">
+    <div className='expense-item'>
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
       </div>
   );
+}
   //components are functions that return HTML code
   //here we write html code that nicely displays info for date etc
   // One root element per return statement if you do this:
@@ -32,7 +25,6 @@ function ExpenseItem(props) {
   // it will give error
   // the way to get around that is to wrap everything in a div 
         // now we can hve multiple elements that can also be side by side
-}
 
 // we must export it so it is usable outside the file
 export default ExpenseItem;
