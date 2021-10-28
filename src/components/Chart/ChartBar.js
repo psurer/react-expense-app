387597c9d93742188536915c729d17da33f7d566
psurer <surereker@icloud.com>
2021-10-28 02:17:47 +0200

    adding chartBar component

---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./ChartBar.css";
+
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  // here we get the percentage between 0-100 by which the bar should be filled
+  // we convert to string with percentage sign at the end
+  if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100 + "%");
+  }
+  // style wants an object
+  // the double {{}} is to output something dynamically and inside is an JS object which is created with {}
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
